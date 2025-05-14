d17ca1eb8ec01c19b60ee2ade14a50630f090958
jscheth <sovichea.chet@gmail.com>
2025-05-14 14:34:41 -0700

    Initial Commit

---
+++ StockMoji/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockMoji
+//
+//  Created by Jonathan Cheth on 5/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StockMojiTests/StockMojiTests.swift
@@ -0,0 +1,17 @@
+//
+//  StockMojiTests.swift
+//  StockMojiTests
+//
+//  Created by Jonathan Cheth on 5/14/25.
+//
+
+import Testing
+@testable import StockMoji
+
+struct StockMojiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
